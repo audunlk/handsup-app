b2c9f1e368760c5eb6b314e3e5784657535e4e62
audunlk <audunlk@gmail.com>
2023-04-18 14:40:00 +0200

    added constants to destroy keys

--- handsup-frontend/firebase/firebase.tsx
+++ handsup-frontend/firebase/firebase.tsx
@@ -1,15 +1,16 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import Constants from 'expo-constants';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD4k_ZA3GR4m8cLf9Fqf0bVY5rlP5oOxOA",
-  authDomain: "handsup-52ea9.firebaseapp.com",
-  projectId: "handsup-52ea9",
-  storageBucket: "handsup-52ea9.appspot.com",
-  messagingSenderId: "670249552437",
-  appId: "1:670249552437:web:f4633706bfb07938fa7f7c",
-  measurementId: "G-4R647NT273"
+  apiKey: Constants.manifest.extra.apiKey,
+  authDomain: Constants.manifest.extra.authDomain,
+  projectId: Constants.manifest.extra.projectId,
+  storageBucket: Constants.manifest.extra.storageBucket,
+  messagingSenderId: Constants.manifest.extra.messagingSenderId,
+  appId: Constants.manifest.extra.appId,
+  measurementId: Constants.manifest.extra.measurementId
 };
 
 
